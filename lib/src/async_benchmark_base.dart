f12fb8e7859d129165f939eb016c5526646fd8aa
gmpassos <gracilianomp@gmail.com>
2024-12-02 04:15:30 -0300

    Documentation

--- lib/src/async_benchmark_base.dart
+++ lib/src/async_benchmark_base.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:isolate';
 
+/// Type for setup value and shutting down benchmarks service on an [Isolate].
 typedef BenchmarkOnIsolateSetup<S> = ({
   S setup,
   Future<void> Function() shutdown
@@ -11,26 +12,34 @@ typedef BenchmarkOnIsolateSetup<S> = ({
 /// of benchmarks executed on a separate [Isolate].
 class _BenchmarkOnIsolate<S, O, B extends Benchmark<S, O>>
     extends Benchmark<BenchmarkOnIsolateSetup<S>, O> {
+  /// The benchmark instance that this class is managing.
   final B benchmark;
 
   _BenchmarkOnIsolate(this.benchmark) : super(benchmark.title);
 
+  /// Sets up the benchmark on the [Isolate], returning a [BenchmarkSetupResult]
+  /// containing setup data and service information.
   @override
   Future<BenchmarkSetupResult<BenchmarkOnIsolateSetup<S>, O>> setup() async {
     var r = await _runSetupOnIsolate(benchmark);
     return (setup: r, service: null);
   }
 
+  /// Shuts down the benchmark by calling the [shutdown] method on both the
+  /// [Isolate] and the benchmark itself.
   @override
   Future<void> shutdown(BenchmarkOnIsolateSetup<S> setup, O? service) async {
     await benchmark.shutdown(setup.setup, service);
     await setup.shutdown();
   }
 
+  /// Executes the benchmark job.
   @override
   FutureOr<void> job(BenchmarkOnIsolateSetup<S> setup, O? service) =>
       benchmark.job(setup.setup, service);
 
+  /// Teardown after running the job, by invoking the [teardown] method of the
+  /// benchmark.
   @override
   FutureOr<void> teardown(BenchmarkOnIsolateSetup<S> setup, O? service) =>
       benchmark.teardown(setup.setup, service);
@@ -101,24 +110,38 @@ class _BenchmarkOnIsolate<S, O, B extends Benchmark<S, O>>
   }
 }
 
+/// Represents a profile configuration for benchmarking.
 class BenchmarkProfile {
+  /// A pre-defined benchmarking profile for instant/quick performance tests
+  /// to ensure the benchmark runs successfully.
   static final instant =
       const BenchmarkProfile('instant', warmup: 1, interactions: 1, rounds: 1);
 
+  /// A pre-defined benchmarking profile for fast performance tests.
   static final fast =
       const BenchmarkProfile('fast', warmup: 10, interactions: 100, rounds: 1);
 
+  /// A pre-defined benchmarking profile for normal performance tests.
   static final normal = const BenchmarkProfile('normal',
       warmup: 100, interactions: 1000, rounds: 3);
 
+  /// A pre-defined benchmarking profile for heavy performance tests.
   static final heavy = const BenchmarkProfile('heavy',
       warmup: 1000, interactions: 10000, rounds: 10);
 
+  /// The name of the benchmark profile.
   final String name;
+
+  /// The number of warmup iterations for the benchmark.
   final int warmup;
+
+  /// The number of interactions per benchmark round.
   final int interactions;
+
+  /// The number of rounds for the benchmark.
   final int rounds;
 
+  /// Constructs a custom [BenchmarkProfile].
   const BenchmarkProfile(this.name,
       {required this.warmup, required this.interactions, required this.rounds});
 
@@ -127,25 +150,37 @@ class BenchmarkProfile {
       'BenchmarkProfile[$name]{warmup: $warmup, interactions: $interactions, rounds: $rounds}';
 }
 
+/// Result type for benchmark setup, consisting of setup data and an optional service.
+/// The [service], if defined, won't be shared between [Isolate]s.
 typedef BenchmarkSetupResult<S, O> = ({S setup, O? service});
 
+/// Abstract class that defines the core structure of a benchmark test.
 abstract class Benchmark<S, O> {
+  /// The title or name of the benchmark.
   final String title;
 
+  /// Constructs a [Benchmark].
   const Benchmark(this.title);
 
+  /// Sets up the benchmark, returning a result that includes setup data and an optional service.
+  /// The [service], if defined, won't be shared between [Isolate]s.
   FutureOr<BenchmarkSetupResult<S, O>> setup() async {
     return (setup: null as S, service: null);
   }
 
+  /// Executes the benchmark job with the provided setup and service.
   FutureOr<void> job(S setup, O? service);
 
+  /// Performs any necessary teardown after the benchmark job iterations are executed.
   FutureOr<void> teardown(S setup, O? service) async {}
 
+  /// Shuts down the benchmark by cleaning up any resources.
+  /// When running in an [Isolate], the [service] should be finalized.
   FutureOr<void> shutdown(S setup, O? service) async {}
 }
 
 extension BenchmarkExtension<S, O, B extends Benchmark<S, O>> on B {
+  /// Runs the benchmark with the specified profile and options.
   Future<BenchmarkResult<S, O, B>> run({
     BenchmarkProfile? profile,
     int? warmup,
@@ -163,6 +198,7 @@ extension BenchmarkExtension<S, O, B extends Benchmark<S, O>> on B {
 
 extension IterableBenchmarkExtension<S, O, B extends Benchmark<S, O>>
     on Iterable<B> {
+  /// Runs all benchmarks in the iterable collection with the specified options.
   Future<List<BenchmarkResult<S, O, B>>> runAll({
     BenchmarkProfile? profile,
     int? warmup,
@@ -238,6 +274,7 @@ extension IterableBenchmarkExtension<S, O, B extends Benchmark<S, O>>
 
 extension MapEntryBenchmarkResultsExtension<S, O, B extends Benchmark<S, O>>
     on MapEntry<B, List<BenchmarkRoundResult>> {
+  /// Returns the entry with the best performance (higher Hertz value).
   MapEntry<B, List<BenchmarkRoundResult>> best(
       MapEntry<B, List<BenchmarkRoundResult>> other) {
     var b1 = value.best;
@@ -253,9 +290,13 @@ extension MapEntryBenchmarkResultsExtension<S, O, B extends Benchmark<S, O>>
   }
 }
 
+/// A structure that contains the results of a benchmark execution.
 class BenchmarkResult<S, O, B extends Benchmark<S, O>>
     implements Comparable<BenchmarkResult<S, O, B>> {
+  /// The [Benchmark] that was executed.
   final B benchmark;
+
+  /// The results of the execution rounds ([BenchmarkRoundResult]).
   final List<BenchmarkRoundResult> rounds;
 
   BenchmarkResult(this.benchmark, this.rounds);
@@ -267,6 +308,7 @@ class BenchmarkResult<S, O, B extends Benchmark<S, O>>
     return b1.compareTo(b2);
   }
 
+  /// Returns the [BenchmarkResult] with the best performance (higher Hertz value).
   BenchmarkResult<S, O, B> best(BenchmarkResult<S, O, B> other) =>
       compareTo(other) <= 0 ? this : other;
 
@@ -280,24 +322,35 @@ extension IterableBenchmarkResultExtension<S, O, B extends Benchmark<S, O>,
   R get best => reduce((a, b) => a.best(b) as R);
 }
 
+/// Represents a single round of a [Benchmark], including performance details.
 class BenchmarkRoundResult implements Comparable<BenchmarkRoundResult> {
+  /// The round number for the [Benchmark].
   final int round;
+
+  /// The initial time of the [Benchmark] round.
   final DateTime initTime;
+
+  /// The end time of the [Benchmark] round.
   final DateTime endTime;
 
+  /// The number of job interactions executed.
   final int interactions;
 
+  /// The total duration of the round execution.
   late final Duration duration = endTime.difference(initTime);
 
+  /// The Hertz value representing the performance ([interactions] per second).
   late final double hertz =
       (interactions / duration.inMicroseconds) * (1000 * 1000);
 
+  /// The average time per job interaction in milliseconds.
   late final double interactionTimeMs =
       (duration.inMicroseconds / interactions) / 1000;
 
   BenchmarkRoundResult(
       this.round, this.initTime, this.endTime, this.interactions);
 
+  /// A formatted string representing the [hertz] value.
   String get hertzFormatted {
     var hz = hertz;
 
@@ -312,6 +365,7 @@ class BenchmarkRoundResult implements Comparable<BenchmarkRoundResult> {
     }
   }
 
+  /// A formatted string representing the [interactionTimeMs].
   String get interactionTimeFormatted {
     var ms = interactionTimeMs;
 
@@ -341,6 +395,7 @@ class BenchmarkRoundResult implements Comparable<BenchmarkRoundResult> {
   @override
   int compareTo(BenchmarkRoundResult other) => other.hertz.compareTo(hertz);
 
+  /// Returns the [BenchmarkRoundResult] with the best performance (higher Hertz value).
   BenchmarkRoundResult best(BenchmarkRoundResult other) =>
       compareTo(other) <= 0 ? this : other;
 
@@ -350,9 +405,28 @@ class BenchmarkRoundResult implements Comparable<BenchmarkRoundResult> {
 }
 
 extension BenchmarkRoundResultExtension on Iterable<BenchmarkRoundResult> {
+  /// Returns the [BenchmarkRoundResult] with the best performance (higher Hertz value).
   BenchmarkRoundResult get best => reduce((a, b) => a.best(b));
 }
 
+/// Runs a benchmark with the specified configuration, executing the setup,
+/// job, and teardown, and returns the [BenchmarkResult] including performance
+/// details like round duration, Hertz and interaction time.
+///
+/// Parameters:
+/// - [benchmark]: The benchmark instance to run. It must extend the [Benchmark] class.
+/// - [profile]: An optional [BenchmarkProfile] to define the number of rounds, warmups,
+///   and interactions (default: [BenchmarkProfile.normal]).
+/// - [warmup]: An optional number of warmup iterations before the actual benchmark starts.
+///   This overrides the [profile] warmup setting if provided.
+/// - [interactions]: An optional number of interactions to perform in each benchmark round.
+///   This overrides the [profile] interactions setting if provided.
+/// - [setupOnIsolate]: A flag indicating whether to run the benchmark setup on a separate [Isolate].
+///   Defaults to `false`, meaning the setup runs on the same [Isolate] as the job interactions.
+/// - [verbose]: If `true`, prints detailed output during execution (default is `false`).
+///
+/// Returns a [BenchmarkResult] containing the benchmark performance metrics
+/// (e.g., duration, interaction time) for each round.
 Future<BenchmarkResult<S, O, B>> benchmark<S, O, B extends Benchmark<S, O>>(
   B benchmark, {
   BenchmarkProfile? profile,
